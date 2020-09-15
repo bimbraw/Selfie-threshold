d81e3731f38dace9dd055afb6a6abb2e142fadd1
Keshav Bimbraw <bimbrawkeshav@gmail.com>
2020-09-15 18:29:05 -0400

    Added two cases for brightness based thresholding

--- Selfie_threshold/Selfie_threshold.cpp
+++ Selfie_threshold/Selfie_threshold.cpp
@@ -1,20 +1,50 @@
 // Selfie_threshold.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+Mat img_gray;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+    //just testing
+
+    // Read older selfie
+    Mat src = imread("C:/Users/Keshav Bimbraw/source/repos/Selfie_threshold/selfie.jpg", IMREAD_GRAYSCALE);
+    Mat dst;
+
+    //cout << src.size();
+
+    // Set threshold and maxValue
+    double thresh = 128;
+    double maxValue = 255;
+
+    // Binary Threshold
+    threshold(src, dst, thresh, maxValue, THRESH_BINARY);
+
+    namedWindow("image", WINDOW_AUTOSIZE);
+    imshow("image", dst);
+    waitKey(0);//esc
+
+    //since this selfie is very dark, I took a new selfie named selfie_new.jpg
+    // Read new selfie
+    Mat src_1 = imread("C:/Users/Keshav Bimbraw/source/repos/Selfie_threshold/selfie_new.jpg", IMREAD_GRAYSCALE);
+    Mat dst_1;
+
+    // Set threshold and maxValue
+    double thresh_1 = 128;
+    double maxValue_1 = 255;
+
+    // Binary Threshold
+    threshold(src_1, dst_1, thresh_1, maxValue_1, THRESH_BINARY);
+
+    namedWindow("image", WINDOW_AUTOSIZE);
+    imshow("image", dst_1);
+    waitKey(0);//esc
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
